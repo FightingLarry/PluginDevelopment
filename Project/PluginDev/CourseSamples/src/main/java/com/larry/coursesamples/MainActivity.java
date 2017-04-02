16b7ecf6a8bfab19a7148deae7d49dc6f1f4a00e
DemonHunter <511573633@qq.com>
2017-04-02 18:48:08 +0800

    demo

---
+++ Project/PluginDev/CourseSamples/src/main/java/com/larry/coursesamples/MainActivity.java
@@ -0,0 +1,16 @@
+package com.larry.coursesamples;
+
+import com.larry.light.LightFragmentActivity;
+
+public class MainActivity extends LightFragmentActivity {
+
+
+    @Override
+    protected void initializeStartingFragment() {
+        try {
+            loadFragment(new MainFragment(), null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Project/PluginDev/CourseSamples/src/main/java/com/larry/coursesamples/MainInfo.java
@@ -0,0 +1,18 @@
+package com.larry.coursesamples;
+
+/**
+ * Created by Larry on 2017/4/2.
+ */
+
+public class MainInfo {
+
+    private String context;
+
+    public String getContext() {
+        return context;
+    }
+
+    public void setContext(String context) {
+        this.context = context;
+    }
+}
