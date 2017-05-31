5548e128eba55654759e8a99a9e3bd95cea31c59
Larry <511573633@qq.com>
2017-05-31 20:35:26 +0800

    添加 轻应用框架

--- Project/PluginDev/lite/host/src/main/java/com/larry/lite/host/MainActivity.java
+++ Project/PluginDev/lite/host/src/main/java/com/larry/lite/host/MainActivity.java
@@ -6,6 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.larry.lite.LiteEvent;
+import com.larry.lite.LiteManager;
+import com.larry.lite.LitePluginSDK;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btn1;
@@ -28,9 +32,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn1:
+                LitePluginSDK.pumpEvent(getApplicationContext(), LiteEvent.Periodicity);
                 break;
             case R.id.btn2:
-
+                LitePluginSDK.pumpEvent(getApplicationContext(), LiteEvent.KeyEventStart);
+                break;
+            case R.id.btn3:
+                LitePluginSDK.pumpEvent(getApplicationContext(), LiteEvent.KeyEventUpgrade);
+                break;
+            case R.id.btn4:
+                LitePluginSDK.pumpEvent(getApplicationContext(), LiteEvent.KeyEventBackground);
+                break;
+            case R.id.btn5:
+                LitePluginSDK.pumpEvent(getApplicationContext(), LiteEvent.KeyEventImmediate);
+                break;
+            case R.id.btn6:
+                LitePluginSDK.pumpEvent(getApplicationContext(), LiteEvent.KeyEventDebug);
+                break;
+            case R.id.btn7:
+                LitePluginSDK.pumpEvent(getApplicationContext(), LiteEvent.KeyEventDumpHPROF);
                 break;
         }
     }
